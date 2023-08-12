89f3ca2cf98af9e954efae57bd174d8826f459d8
Habeeb <habeebav841@gmail.com>
2023-08-12 17:55:29 +0530

    commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const router = express.Router()
+
+const {
+    getPosts,
+    getPost,
+    addPost,
+    deletePost,
+    updatePost,
+    searchPost,
+    likePost,
+    commentPost
+     } = require('../controllers/postController')
+
+router.get("/", getPosts)
+router.get("/:id", getPost)
+router.post("/", addPost)
+router.delete("/:id", deletePost)
+router.put("/:id", updatePost)
+router.get('/searchPost/:key',searchPost)
+
+router.patch("/:id/like", likePost);
+router.patch("/:id/comment", commentPost);
+
+
+module.exports = router
